040a5f9ab32ddfac6492efa34e2de9297f4013cb
Blas <slab10000@gmail.com>
2025-10-26 23:55:31 -0500

    Create a simple component

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,13 @@
+// .tsx for React Components
+// .ts for plain typescript files
+
+
+// Pascalcase
+function Message(){
+    // JSX: Javascript XML -> gets compiled to javascript
+    // Here we can se how it is compiled: https://babeljs.io
+    return <h1>Hello world </h1>;
+
+}
+
+export default Message;

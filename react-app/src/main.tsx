41aa9395d375aea524be952127a345c9c72a270b
Blas <slab10000@gmail.com>
2025-10-27 00:23:16 -0500

    Bootstrap installation files

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css' --> we are deleting this file
-import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/css/bootstrap.css" // Add bootstrap - we see that our page changes
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(

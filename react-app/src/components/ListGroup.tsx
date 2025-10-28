caa201e8f8e2e0b42b59f156410fbd0ed61ed788
Blas <slab10000@gmail.com>
2025-10-28 00:47:03 -0500

    Fragment
    We can group elements under a Fragment

---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+
+function ListGroup() {
+  return (
+    // Note that we need this braket to use multiple lines
+
+    <Fragment>
+      <h1>This is my title</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </Fragment>
+  );
+}
+
+export default ListGroup;

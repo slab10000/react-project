ac8b7b742308675161baba822fc3244b2dac53ed
Blas <slab10000@gmail.com>
2025-10-28 22:05:19 -0500

    Ways of dynamically show or hide elements

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,9 +1,40 @@
 function ListGroup() {
-	const items = ["Madrid", "Paris", "Rome", "Tokyo"];
+	var items = ["Madrid", "Paris", "Rome", "Tokyo"];
+	items = [];
+
+	/* We can use this logic to render it dynamically but repeats code
+    
+    if (items.length === 0)
+		return (
+			<>
+				<h1>Title</h1>
+				<p>No items found</p>
+			</>
+		); 
+    */
+
+	/* Creating a variable: */
+	const message = items.length === 0 ? <p>No items found</p> : null;
+
+	/* Using a function if we can is better since we can use parameters */
+	const getMessage = () => {
+		return items.length === 0 ? <p>No items found</p> : null;
+	};
+
+	/* Remember, in js, the logic behind && is that if it is true it returns the second part of the comparation */
+	true && "Blas"; // Returns Blas
+	false && "Blas"; // Returns false
+	{
+		items.length === 0 && <p>No items found</p>;
+	} // Returns false or the value of the paragraph
 
 	return (
 		<>
 			<h1>This is my title</h1>
+			{message}
+			{getMessage()}
+			{items.length === 0 ? <p>No items found</p> : null /* Example 1 */}
+			{items.length === 0 && <p>No items found</p> /* Example 2 */}
 			<ul className="list-group">
 				{items.map((item) => (
 					<li key={item}>{item}</li>
